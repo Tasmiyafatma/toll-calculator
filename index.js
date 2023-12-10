f7584d8370104ad407e0075357ed0cbbca6e9ec4
tasmiya.parveen <tasmiya.parveen@jukshio.com>
2023-12-10 22:40:01 +0530

    comments added to js code

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let polylineData;
 
 const form = document.getElementById("myForm");
 
-// Add an event listener to the form's submit event
+// adding a submit even handler so it will trigger when we click the submit button
 form.addEventListener("submit", function (event) {
-  // Prevent the default form submission
+  // Prevent the default form submission or else the page is going to get refresh
   event.preventDefault();
 
   // Collect form data
@@ -14,19 +14,20 @@ form.addEventListener("submit", function (event) {
     endAddress: document.getElementById("end-address").value,
   };
 
-  // Do something with the form data (e.g., log it to the console)
+  // logging the data in the console
   console.log(formData);
 
-  // You can perform further actions with the form data here
+  // fetching the using toll guru api by passing the start address and end address
   fetchData(formData.startAddress, formData.endAddress);
 });
 
 async function fetchData(startAddress, endAddress) {
   const url =
     "https://apis.tollguru.com/toll/v2/origin-destination-waypoints/#";
-  // const apiKey = "mR3hbhRpMFRDQfTMb8qp9gpGnjHQmf69";
+  // api key to get data from toll guru api
   const apiKey = "nrtq7tT4QjNgbdD9pQJmtjrnnJb2hGqT";
 
+  // start address and end address
   const data = {
     from: {
       address: startAddress,
@@ -41,6 +42,7 @@ async function fetchData(startAddress, endAddress) {
   };
 
   try {
+    // making api call using fetch parameters are api url, method POST, headers and body 
     const response = await fetch(url, {
       method: "POST",
       headers: {
@@ -54,6 +56,7 @@ async function fetchData(startAddress, endAddress) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
+    // we will get the data from api assigning to routeData variable
     routeData = await response.json();
     console.log(routeData);
     displayMap(routeData.routes[0].summary.url);
@@ -61,6 +64,8 @@ async function fetchData(startAddress, endAddress) {
     displayDistanceText(routeData.routes[0].summary.distance.metric);
     displayExpressWayText(routeData.routes[0].summary.name);
     displayTollCostText(routeData.routes[0].costs.minimumTollCost);
+
+    // passing the encoded polyline data to get the roll information
     polylineData = await getTollInformation(routeData.routes[0].polyline);
     console.log(polylineData);
   } catch (error) {
@@ -68,7 +73,10 @@ async function fetchData(startAddress, endAddress) {
   }
 }
 
+
+// this function I am making api call to get toll information by pasing the polyline
 async function getTollInformation(polylineData) {
+  // api key to get data from toll guru api
   const apiKey = "nrtq7tT4QjNgbdD9pQJmtjrnnJb2hGqT";
   const apiUrl =
     "https://apis.tollguru.com/toll/v2/complete-polyline-from-mapping-service";
@@ -95,27 +103,32 @@ async function getTollInformation(polylineData) {
   return await response.json();
 }
 
+// to display map using data we got from toll guru api
 async function displayMap(url) {
   const iframeElement = document.getElementById("map-iframe");
 
   iframeElement.src = url;
 }
 
+// to display the distance of two way points using id
 async function displayDistanceText(text) {
   const distanceText = document.getElementById("distance-text");
   distanceText.textContent = text;
 }
 
+// to display the total duration
 async function displayDurationText(text) {
   const durationText = document.getElementById("duration-text");
   durationText.textContent = text;
 }
 
+// to display the express of the way the route will be
 async function displayExpressWayText(text) {
   const expressWayText = document.getElementById("express-way-text");
   expressWayText.textContent = text;
 }
 
+// to display total cost need to be paid by the traveller
 async function displayTollCostText(text) {
   const expressWayText = document.getElementById("toll-cost-text");
   expressWayText.textContent = text;
